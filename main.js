8e67e163f44ef84f5cf185545b9cf6e0d5d5e921
Fabian <fabrofabian@gmail.com>
2025-11-17 19:24:41 -0800

    added trees

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import * as THREE from "three";
 
+const bamboos = [];
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -13,21 +15,33 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setAnimationLoop(animate);
 document.body.appendChild(renderer.domElement);
 
-const planeGeometry = new THREE.PlaneGeometry(7, 7);
+const planeGeometry = new THREE.PlaneGeometry(10, 10);
 const planeMaterial = new THREE.MeshBasicMaterial({
   color: 0xcccccc
 });
 const plane = new THREE.Mesh(planeGeometry, planeMaterial);
 scene.add(plane);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-geometry.translate(0, 0, 0.5);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+for(let i = 0; i < 10; i++) {
+  const randomX = Math.random(0, 85);
+  const randomY = Math.random(0, 55);
+  
+  const geometry = new THREE.BoxGeometry(0.5, 0.5, 8);
+  geometry.translate(1, 2, 10);
 
-camera.position.z = 5;
-camera.position.y = 7;
+  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+  const cube = new THREE.Mesh(geometry, material);
+  bamboos.push(cube);
+  // scene.add(cube);
+
+}
+
+for(let tree of bamboos){
+  scene.add(tree);
+}
+
+camera.position.z = 10;
+camera.position.y = 10;
 camera.up.set(0, 0, 1);
 camera.lookAt(0, 0, 0);
 
